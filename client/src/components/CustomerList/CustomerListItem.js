c739aa719aa6db9650c2fd22749bcec4b94db6a2
Mike Hunsicker <mshunts@gmail.com>
2020-05-24 16:25:47 -0500

    Add customer delete and edit

--- client/src/components/CustomerList/CustomerListItem.js
+++ client/src/components/CustomerList/CustomerListItem.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { slugify } from 'slugify'
 import styled from 'styled-components'
+import { Button } from '../Button'
 
 const Container = styled.div`
   padding: 5px 20px;
 `
 
-export const CustomerListItem = ({ customer }) => {
+const Controls = styled.div`
+  margin: 10px 0 10px 0;
+  & button {
+    margin-left: 20px;
+  }
+`
+
+export const CustomerListItem = ({ customer, deleteCustomer, editCustomer }) => {
+  const history = useHistory()
+
+  const handleEditCustomer = customer => {
+    editCustomer(customer)
+    history.push(`edit-customer/${customer._id}`)
+  }
+
   return (
     <Container>
       Customer: {customer.name}
+      <Controls>
+        <Button onClick={() => handleEditCustomer(customer)}>Edit</Button>
+        <Button onClick={() => deleteCustomer(customer)}>Delete</Button>
+      </Controls>
     </Container>
   )
 }
