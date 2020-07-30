273f14367ba4a32039714540d33fff698d4d4ac5
Mike Hunsicker <mshunts@gmail.com>
2020-07-30 06:56:03 -0500

    Added TaskList component stub

---
+++ client/src/components/TaskList/TaskList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Flex, Box } from 'rebass/styled-components'
+import styled from 'styled-components'
+import { Text } from '../Text'
+
+export const TaskList = props => {
+  const Wrapper = styled(Box)`
+    margin: 5px 0;
+  `
+  const List = styled.ol`
+    padding-left: 20px;
+    margin-top: 0;
+  `
+  const tasks = [
+    'Call back customer Joe',
+    'Write up an estimate for customer Betty',
+    'Pick up supplies from Bliffert',
+    'Get stuff done',
+    'Give random bonus to web developer'
+  ]
+
+  return (
+    <Flex flexDirection="column">
+    <Text as='h3'>Top Tasks</Text>
+    <List>
+    {
+      tasks.map((task, i) => (
+        <Wrapper key={i} as="li">
+          <Text>{task}</Text>
+        </Wrapper>
+      ))
+    }
+    </List>
+    </Flex>
+  )
+}
