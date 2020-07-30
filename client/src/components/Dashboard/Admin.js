003cff640802ae5d8c35ae03974beadd4aa0e8ff
Mike Hunsicker <mshunts@gmail.com>
2020-07-30 06:57:21 -0500

    Added Financials and TaskList components to Dashboard

--- client/src/components/Dashboard/Admin.js
+++ client/src/components/Dashboard/Admin.js
@@ -1,4 +1,23 @@
 import React from 'react'
+import { Flex, Box } from 'rebass'
 import { ProductionCalendar } from '../Calendar'
+import { Financials } from '../Financials'
+import { TaskList } from '../TaskList'
 
-export const Admin = () => <ProductionCalendar />
+export const Admin = () => (
+  <Flex>
+    <Box width={[1, 1, 3/4]}>
+      <ProductionCalendar />
+    </Box>
+    <Box width={[1, 1, 1/4]} p={3}>
+      <Flex flexDirection="column">
+        <Box>
+          <Financials />
+        </Box>
+        <Box>
+          <TaskList />
+        </Box>
+      </Flex>
+    </Box>
+  </Flex>
+)
